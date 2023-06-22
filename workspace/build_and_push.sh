daaadb0718e12e168785c92f66b447d80d6a34e4
Corentin LE GALL <clegall@aneo.fr>
2023-06-22 17:19:25 +0200

    update

--- workspace/build_and_push.sh
+++ workspace/build_and_push.sh
@@ -1,14 +1,18 @@
+#!/bin/bash
+
 mkdir docker
 
-%%writefile docker/dockerfile
+cd docker
 
-FROM python:3.7-slim-buster
-RUN pip3 install pandas scikit-learn
-ENV PYTHONUNBUFFERED=TRUE
-COPY processing.py .
+touch dockerfile
+echo "FROM python:3.7-slim-buster" > dockerfile
+echo "RUN pip3 install pandas scikit-learn" > dockerfile
+echo "ENV PYTHONUNBUFFERED=TRUE" > dockerfile
+echo "COPY processing.py ." > dockerfile
 
-!docker build -t $ecr_repository docker
-!aws ecr get-login-password --region {region} | docker login --username AWS --password-stdin {account_id}.dkr.ecr.{region}.amazonaws.com
-!aws ecr create-repository --repository-name $ecr_repository
-!docker tag {ecr_repository + tag} $processing_repository_uri
-!docker push $processing_repository_uri
+
+docker build -t $ecr_repository docker
+aws ecr get-login-password --region {region} | docker login --username AWS --password-stdin {account_id}.dkr.ecr.{region}.amazonaws.com
+aws ecr create-repository --repository-name $ecr_repository
+docker tag {ecr_repository + tag} $processing_repository_uri
+docker push $processing_repository_uri
